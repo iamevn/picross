5a23ffa735a12ad64c2e733ad381c516d66a2490
iamevn <iamevn@users.noreply.github.com>
2017-05-18 04:58:26 -0400

    grid generation added (using placeholder puzzles)

--- scripts/main.js
+++ scripts/main.js
@@ -79,6 +79,111 @@ puzzle.addEventListener("mouseup", function (event) {
   which = false;
 }, false);
 
-/* fill out table element on generate */
-var genBtn = document.getElementById("generate");
+/* fill out table when generate is clicked */
 var sizeSelector = document.getElementById("size");
+var genBtn = document.getElementById("generate");
+var clearBtn = document.getElementById("clear");
+
+/* placeholder puzzles until I can generate or pull puzzles from a big file */
+var placeholders = {
+  "5x5" : [
+    [5, 5], // size (width, height)
+    [[1], [2, 1], [1], [2, 1], [1]], // top hints
+    [[1, 1], [1, 1], [0], [1, 1], [3]] //side hints
+  ],
+  "10x10": [
+    [10, 10],
+    [[5, 4], [5, 1, 1], [1, 1, 1, 1, 1], [1, 1, 4], [0],
+     [3, 4], [2, 2], [2, 2], [2, 2], [3, 4]],
+    [[4, 1, 1], [2, 1, 1], [3, 2, 2], [2, 3], [4, 1],
+     [0], [4, 2, 1], [1, 1, 3, 1], [4, 1, 3], [1, 1, 1, 2]]
+  ],
+  "15x15": [
+    [15, 15],
+    [[0], [4], [6], [8], [9], [10],
+     [10], [10], [2, 7], [2, 5],
+     [3, 3], [2, 3], [2, 3], [4], [0]],
+    [[0], [0], [3, 3], [5, 5], [8, 1, 2],
+     [7, 1], [8, 2], [9, 3], [11], [9],
+     [7], [5], [3], [1], [0]]
+  ],
+  "xd" : [
+    [11, 11],
+    [[1], [1, 4], [5], [1, 4], [1], [11],
+      [1], [5, 4], [1, 1], [5, 1], [1, 1]],
+    [[1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [11],
+      [1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 4]]
+  ]
+};
+genBtn.addEventListener("mouseup", function (event) {
+  var puzz = placeholders[sizeSelector.value];
+  var width = puzz[0][0];
+  var height = puzz[0][1];
+  var topClues = puzz[1];
+  var sideClues = puzz[2];
+
+  puzzle.innerHTML = "";
+
+  // header row
+  var row = puzzle.insertRow();
+  var headerCell = document.createElement("th");
+  row.appendChild(headerCell);
+  for (var j = 0; j < width; j++) {
+    headerCell = document.createElement("th");
+    headerCell.id = "t" + j;
+    headerCell.className = "top";
+    headerCell.scope = "col";
+    // fill in clue
+    var s = ""
+    topClues[j].forEach(function (clue) {
+      if (s !== "") {
+        s = s + "</br>";
+      }
+      s = s + clue;
+    });
+    headerCell.innerHTML = s;
+    row.appendChild(headerCell);
+  }
+
+  // main rows
+  for (var i = 0; i < height; i++) {
+    row = puzzle.insertRow();
+    headerCell = document.createElement("th");
+    headerCell.id = "s" + i;
+    headerCell.className = "side";
+    headerCell.scope = "row";
+    // fill in clue
+    var s = ""
+    sideClues[i].forEach(function (clue) {
+      if (s !== "") {
+        s = s + " ";
+      }
+      s = s + clue;
+    });
+    headerCell.innerHTML = s;
+    row.appendChild(headerCell);
+
+    for (var j = 0; j < width; j++) {
+      var cell = row.insertCell();
+      cell.className = "empty";
+      cell.id = i + "-" + j;
+    }
+  }
+}, false);
+
+/* clear all cells */
+clearBtn.addEventListener("mouseup", function (event) {
+  var puzz = placeholders[sizeSelector.value];
+  var width = puzz[0][0];
+  var height = puzz[0][1];
+
+  var cell;
+  for (var i = 0; i < width; i++) {
+    for (var j = 0; j < width; j++) {
+      cell = document.getElementById(i + "-" + j);
+      if (cell) {
+        cell.className = "empty";
+      }
+    }
+  }
+}, false);
